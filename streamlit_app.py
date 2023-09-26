062a8989e487131904da4f6a1309bbf044459e66
hetaludani <31139049+hetaludani@users.noreply.github.com>
2023-09-26 17:13:39 -0500

    Update streamlit_app.py
    Added 3 Lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
 streamlit.title('My Parents First Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Kale,Spinach Quiche, Avocado Toast')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
