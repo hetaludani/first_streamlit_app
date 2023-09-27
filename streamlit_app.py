1043652141f56627dae37cd07208f38f8ec9de7d
hetaludani <31139049+hetaludani@users.noreply.github.com>
2023-09-27 10:08:29 -0500

    Update streamlit_app.py
    Added json call

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,4 +14,4 @@ my_fruit_show=my_fruit_list.loc[my_fruit_selected]
 streamlit.dataframe(my_fruit_show)
 import requests
 fruityvice_response=requests.get('https://fruityvice.com/api/fruit/watermelon')
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
