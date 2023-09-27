865f2c28846b7d8b726acb3e0911808df4a537b5
hetaludani <31139049+hetaludani@users.noreply.github.com>
2023-09-27 10:19:41 -0500

    Update streamlit_app.py
    Commented a line

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ my_fruit_show=my_fruit_list.loc[my_fruit_selected]
 streamlit.dataframe(my_fruit_show)
 import requests
 fruityvice_response=requests.get('https://fruityvice.com/api/fruit/watermelon')
-streamlit.text(fruityvice_response.json())
+--streamlit.text(fruityvice_response.json())
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
